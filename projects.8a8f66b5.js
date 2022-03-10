2d1d11c750a66c504394d42e8e9ae7368af0459d
baikinkirill <kirill.baikin@gmail.com>
2022-03-10 18:20:42 +0000

    Deploying to gh-pages-pr from @ baikinkirill/Landing-Front@06bbdfb5cdf48f8bee2fce7cbeb0a7b12aa9ec87 🚀

--- projects.8a8f66b5.js
+++ projects.8a8f66b5.js
@@ -125,47 +125,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 var data = [{
-  "title": "ARtillery",
-  "description": "Игровое приложение в дополненной реальности для Android (ранее Android, iOS).\nЗадача игрока – продержаться как можно дольше, отстреливаясь от противников, появляющихся в пространстве вокруг него. Для защиты игрок может перемещать и наклонять телефон, а также сбивать летящие в него снаряды. Для стрельбы нужно коснуться экрана. Со временем сложность увеличивается.\nСуществует три типа противников с разными моделями поведения и сложностью, каждый из которых появляется реже или чаще других.\nПриложение реализовано с использованием ARCore, что позволяет ему работать без привязки к печатной метке.\nРанняя версия приложения была реализована с использованием Vuforia, для работы требуется распечатать метку.",
-  "images": ["https://github.com/RTUITLab/ARtillery/raw/master/ReadmeMedia/ARtillery_Moment3.jpg", "https://github.com/RTUITLab/ARtillery/raw/master/ReadmeMedia/ARtillery_Moment-min.jpg", "https://github.com/RTUITLab/ARtillery/raw/master/ReadmeMedia/ARtillery_Moment2-min.jpg", "https://github.com/RTUITLab/ARtillery/raw/master/ReadmeMedia/ARtilleryAction.gif"],
-  "videos": ["https://www.youtube.com/embed/BIoWlE25bD4"],
-  "tags": ["AR", "Unity"],
-  "tech": ["Unity"],
-  "developers": ["Поздняков Д.В."],
-  "sourceCode": [{
-    "name": "Проект",
-    "link": "https://github.com/RTUITLab/ARtillery"
-  }],
-  "date": "02/03/2022"
-}, {
-  "title": "Многопользовательская сборка ракетного двигателя",
-  "description": "\nПриложение дает пользователям возможность осуществлять процесс сборки предоставленной модели в формате виртуальной реальности. Люди могут собирать ракетный двигатель как самостоятельно, так и в многопользовательской комнате, объединившись с напарниками.\nПроцесс сопровождают различные подсказки и тексты с описаниями деталей. Все это, вкупе с глубоким погружением, способствует быстрому усвоению материала. При окончании сборки модель становится анимированной и демонстрирует свой принцип работы.",
-  "images": ["https://user-images.githubusercontent.com/46666053/141160470-a31d20dd-86bd-4be8-9308-a631227b5ba0.png", "https://user-images.githubusercontent.com/46666053/141155619-bb28a0e8-fd3b-4359-97e5-36f3d1cf71c3.png", "https://user-images.githubusercontent.com/46666053/141155697-f247130d-e9a9-4f7d-812c-7a328e3022af.png", "https://user-images.githubusercontent.com/46666053/141155722-2e5c8eab-dfef-42f7-ae67-daf43e1756d6.png"],
-  "videos": ["https://www.youtube.com/embed/6FTMDRp5fN4"],
-  "tags": ["VR", "Game"],
-  "tech": ["Unity", "C#"],
-  "developers": ["Сиротенко М.Р.", "Корчиков М.Д.", "Смирнов М.А", "Лысаков И.А.", "Смирнов Я.А."],
-  "site": null,
-  "sourceCode": [{
-    "name": "Исходный код",
-    "link": "https://github.com/RTUITLab/Engine-VR"
-  }],
-  "date": "02/03/2022"
-}, {
-  "title": "GyroBall",
-  "description": "Мобильное приложение – игра для проверки удобства использования механики акселерометра.\nИспользование поворотов телефона для управления шариком. У шарика предполагается 4 состояния, соответствующих стихиям, каждое состояние несёт в себе определённую соответствующую механику. Локации к игре будут соответствовать стихийной тематике.",
-  "images": ["https://github.com/RTUITLab/GyroBall/raw/main/landing/23.JPG", "https://github.com/RTUITLab/GyroBall/raw/main/landing/21.JPG", "https://github.com/RTUITLab/GyroBall/raw/main/landing/1.png", "https://github.com/RTUITLab/GyroBall/raw/main/landing/2.png", "https://github.com/RTUITLab/GyroBall/raw/main/landing/3.png"],
-  "videos": [],
-  "tags": ["Mobile"],
-  "tech": ["C#", "3D modeling", "Unity 3D"],
-  "developers": ["Баканова М.В.", "Новаков Н.Ю."],
-  "site": null,
-  "sourceCode": [{
-    "name": "Проект",
-    "link": "https://github.com/RTUITLab/GyroBall"
-  }],
-  "date": "02/03/2022"
-}, {
   "title": "GeoHelper",
   "description": "\nПроект для реализации AR-слоя мира, в котором отображаются объекты дополненной реальности в зависимости от заданного географического положения.\nПроект состоит из сайта и приложений для платформ Android и IOS.\nСайт предназначен для менеджмента данными, представляемых в AR-слое.\nМобильное приложение служит для отображения AR-слоя мира на экране используемого устройства.",
   "images": ["https://github.com/RTUITLab/GeoHelperUnity/raw/master/landing/1.png", "https://github.com/RTUITLab/GeoHelperUnity/raw/master/landing/2.png", "https://github.com/RTUITLab/GeoHelperUnity/raw/master/landing/3.png", "https://github.com/RTUITLab/GeoHelperUnity/raw/master/landing/4.png"],
@@ -186,15 +145,45 @@ var data = [{
   }],
   "date": "02/03/2022"
 }, {
-  "title": "MicroFileServer (файловый сервер для небольших файлов)",
-  "description": "Данный сервис был разработан для добавления к отчетам сотрудников лаборатории\nдополнительных файлов (референсных картинок, видеодемонстраций и т.д.)",
-  "images": ["https://github.com/RTUITLab/ITLab-MFS/raw/master/"],
+  "title": "ARtillery",
+  "description": "Игровое приложение в дополненной реальности для Android (ранее Android, iOS).\nЗадача игрока – продержаться как можно дольше, отстреливаясь от противников, появляющихся в пространстве вокруг него. Для защиты игрок может перемещать и наклонять телефон, а также сбивать летящие в него снаряды. Для стрельбы нужно коснуться экрана. Со временем сложность увеличивается.\nСуществует три типа противников с разными моделями поведения и сложностью, каждый из которых появляется реже или чаще других.\nПриложение реализовано с использованием ARCore, что позволяет ему работать без привязки к печатной метке.\nРанняя версия приложения была реализована с использованием Vuforia, для работы требуется распечатать метку.",
+  "images": ["https://github.com/RTUITLab/ARtillery/raw/master/ReadmeMedia/ARtillery_Moment3.jpg", "https://github.com/RTUITLab/ARtillery/raw/master/ReadmeMedia/ARtillery_Moment-min.jpg", "https://github.com/RTUITLab/ARtillery/raw/master/ReadmeMedia/ARtillery_Moment2-min.jpg", "https://github.com/RTUITLab/ARtillery/raw/master/ReadmeMedia/ARtilleryAction.gif"],
+  "videos": ["https://www.youtube.com/embed/BIoWlE25bD4"],
+  "tags": ["AR", "Unity"],
+  "tech": ["Unity"],
+  "developers": ["Поздняков Д.В."],
+  "sourceCode": [{
+    "name": "Проект",
+    "link": "https://github.com/RTUITLab/ARtillery"
+  }],
+  "date": "02/03/2022"
+}, {
+  "title": "GyroBall",
+  "description": "Мобильное приложение – игра для проверки удобства использования механики акселерометра.\nИспользование поворотов телефона для управления шариком. У шарика предполагается 4 состояния, соответствующих стихиям, каждое состояние несёт в себе определённую соответствующую механику. Локации к игре будут соответствовать стихийной тематике.",
+  "images": ["https://github.com/RTUITLab/GyroBall/raw/main/landing/23.JPG", "https://github.com/RTUITLab/GyroBall/raw/main/landing/21.JPG", "https://github.com/RTUITLab/GyroBall/raw/main/landing/1.png", "https://github.com/RTUITLab/GyroBall/raw/main/landing/2.png", "https://github.com/RTUITLab/GyroBall/raw/main/landing/3.png"],
   "videos": [],
-  "tags": ["Backend"],
-  "tech": ["Go", "MongoDB"],
-  "developers": ["Комар Б.Г."],
+  "tags": ["Mobile"],
+  "tech": ["C#", "3D modeling", "Unity 3D"],
+  "developers": ["Баканова М.В.", "Новаков Н.Ю."],
   "site": null,
-  "sourceCode": [],
+  "sourceCode": [{
+    "name": "Проект",
+    "link": "https://github.com/RTUITLab/GyroBall"
+  }],
+  "date": "02/03/2022"
+}, {
+  "title": "Многопользовательская сборка ракетного двигателя",
+  "description": "\nПриложение дает пользователям возможность осуществлять процесс сборки предоставленной модели в формате виртуальной реальности. Люди могут собирать ракетный двигатель как самостоятельно, так и в многопользовательской комнате, объединившись с напарниками.\nПроцесс сопровождают различные подсказки и тексты с описаниями деталей. Все это, вкупе с глубоким погружением, способствует быстрому усвоению материала. При окончании сборки модель становится анимированной и демонстрирует свой принцип работы.",
+  "images": ["https://user-images.githubusercontent.com/46666053/141160470-a31d20dd-86bd-4be8-9308-a631227b5ba0.png", "https://user-images.githubusercontent.com/46666053/141155619-bb28a0e8-fd3b-4359-97e5-36f3d1cf71c3.png", "https://user-images.githubusercontent.com/46666053/141155697-f247130d-e9a9-4f7d-812c-7a328e3022af.png", "https://user-images.githubusercontent.com/46666053/141155722-2e5c8eab-dfef-42f7-ae67-daf43e1756d6.png"],
+  "videos": ["https://www.youtube.com/embed/6FTMDRp5fN4"],
+  "tags": ["VR", "Game"],
+  "tech": ["Unity", "C#"],
+  "developers": ["Сиротенко М.Р.", "Корчиков М.Д.", "Смирнов М.А", "Лысаков И.А.", "Смирнов Я.А."],
+  "site": null,
+  "sourceCode": [{
+    "name": "Исходный код",
+    "link": "https://github.com/RTUITLab/Engine-VR"
+  }],
   "date": "02/03/2022"
 }, {
   "title": "ITLab (система управления лабораторией)",
@@ -226,6 +215,17 @@ var data = [{
   }],
   "date": "02/03/2022"
 }, {
+  "title": "MicroFileServer (файловый сервер для небольших файлов)",
+  "description": "Данный сервис был разработан для добавления к отчетам сотрудников лаборатории\nдополнительных файлов (референсных картинок, видеодемонстраций и т.д.)",
+  "images": ["https://github.com/RTUITLab/ITLab-MFS/raw/master/"],
+  "videos": [],
+  "tags": ["Backend"],
+  "tech": ["Go", "MongoDB"],
+  "developers": ["Комар Б.Г."],
+  "site": null,
+  "sourceCode": [],
+  "date": "02/03/2022"
+}, {
   "title": "Plane crash simulator",
   "description": "Симулятор, помещающий пользователя в ситуацию, когда он находится в падающем самолете",
   "images": ["https://github.com/RTUITLab/PlaneCrashSimulator/raw/master/landing/1.png", "https://github.com/RTUITLab/PlaneCrashSimulator/raw/master/landing/2.png", "https://github.com/RTUITLab/PlaneCrashSimulator/raw/master/landing/3.png"],
@@ -254,17 +254,6 @@ var data = [{
   }],
   "date": "02/03/2022"
 }, {
-  "title": "Виртуальный ситуационный центр",
-  "description": "Виртуальный ситуационный центр позволяет множеству людей в любой точке планеты взаимодействовать между собой в единой среде виртуальной реальности. При этом возможно использование любого доступного на данный момент шлема виртуальной реальности. Также сервисом можно пользоваться без шлема, используя клавиатуру и мышь.\nВ сервисе реализована концепция комнат, к которым подключаются люди, и их общение происходит только внутри данного пространства. Разговоры и обмен файлами не выйдут за пределы выделенного пространства, что обеспечивает конфиденциальность приватного общения.\nВнутри выделенного виртуального пространства люди могут общаться при помощи голосового чата, могут обмениваться документами из облачных хранилищ (OneDrive, Google drive…), просматривать их, комментировать и обсуждать.\nВ данный момент сервис находится на стадии открытого тестирования.",
-  "images": ["https://files.rtuitlab.dev/landing_src/situation_center/1.png", "https://files.rtuitlab.dev/landing_src/situation_center/2.png"],
-  "videos": ["https://www.youtube.com/embed/HRFRQa3tMcI"],
-  "tags": ["VR", "Social"],
-  "tech": ["NodeJS", "ASP. net core", "Unreal Engine"],
-  "developers": ["Макущенко М.А.", "Иванов И.Е."],
-  "site": null,
-  "sourceCode": [],
-  "date": "02/03/2022"
-}, {
   "title": "Шутер в виртуальной реальности",
   "description": "\nШутер в формате виртуальной реальности, позволяющий оттачивать стрельбу из оружия разных видов и уворачиваться от вражеских пуль.\n\nВ игре можно бросить вызов сразу нескольким противникам, либо сначала потренироваться в спокойной обстановке. В таком режиме у пользователя есть возможность стрелять по бутылкам в баре и мишеням в тире.\n\nНа выбор представлено 5 моделей оружия, 4 локации и 3 уровня сложности.",
   "images": ["https://user-images.githubusercontent.com/57914366/148579235-a4f9e25e-dc3c-49d8-b1a6-7dbabfa52a99.jpg", "https://user-images.githubusercontent.com/57914366/148580147-297f79e2-15e3-4b34-8108-9550982b0fe2.jpg", "https://user-images.githubusercontent.com/57914366/148580160-84ee7b40-83f9-439c-a37e-4e1184bd4dd5.jpg", "https://user-images.githubusercontent.com/57914366/148580059-71f366be-9f6c-44bf-a662-abf7e3e00003.jpg"],
@@ -277,6 +266,17 @@ var data = [{
     "name": "Исходный код",
     "link": "https://github.com/RTUITLab/VRDuel"
   }],
+  "date": "02/03/2022"
+}, {
+  "title": "Виртуальный ситуационный центр",
+  "description": "Виртуальный ситуационный центр позволяет множеству людей в любой точке планеты взаимодействовать между собой в единой среде виртуальной реальности. При этом возможно использование любого доступного на данный момент шлема виртуальной реальности. Также сервисом можно пользоваться без шлема, используя клавиатуру и мышь.\nВ сервисе реализована концепция комнат, к которым подключаются люди, и их общение происходит только внутри данного пространства. Разговоры и обмен файлами не выйдут за пределы выделенного пространства, что обеспечивает конфиденциальность приватного общения.\nВнутри выделенного виртуального пространства люди могут общаться при помощи голосового чата, могут обмениваться документами из облачных хранилищ (OneDrive, Google drive…), просматривать их, комментировать и обсуждать.\nВ данный момент сервис находится на стадии открытого тестирования.",
+  "images": ["https://files.rtuitlab.dev/landing_src/situation_center/1.png", "https://files.rtuitlab.dev/landing_src/situation_center/2.png"],
+  "videos": ["https://www.youtube.com/embed/HRFRQa3tMcI"],
+  "tags": ["VR", "Social"],
+  "tech": ["NodeJS", "ASP. net core", "Unreal Engine"],
+  "developers": ["Макущенко М.А.", "Иванов И.Е."],
+  "site": null,
+  "sourceCode": [],
   "date": "02/03/2022"
 }, {
   "title": "BunnyHuntVR - Web VR шутер",
@@ -705,4 +705,4 @@ gallery.setActiveView(Math.floor(_projectsData.default.length / 2));
 updateProjectsInfo();
 document.getElementById("projects__tags__all").checked = true;
 },{"../data/projectsData":"rSus","../gallery/gallery":"Z6W6"}]},{},["TB2l"], null)
-//# sourceMappingURL=projects.047db069.js.map
+//# sourceMappingURL=projects.8a8f66b5.js.map
