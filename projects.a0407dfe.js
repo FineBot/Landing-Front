54c9124dc5a8cc65d9b2d6996d02a746dffe4fcd
baikinkirill <kirill.baikin@gmail.com>
2022-03-11 15:56:36 +0000

    Deploying to gh-pages-pr from @ baikinkirill/Landing-Front@f1dab444d74a8e82370f037ad4769990a7f0fb23 🚀

--- projects.a0407dfe.js
+++ projects.a0407dfe.js
@@ -226,6 +226,31 @@ var data = [{
   "sourceCode": [],
   "date": "02/03/2022"
 }, {
+  "title": "Виртуальный ситуационный центр",
+  "description": "Виртуальный ситуационный центр позволяет множеству людей в любой точке планеты взаимодействовать между собой в единой среде виртуальной реальности. При этом возможно использование любого доступного на данный момент шлема виртуальной реальности. Также сервисом можно пользоваться без шлема, используя клавиатуру и мышь.\nВ сервисе реализована концепция комнат, к которым подключаются люди, и их общение происходит только внутри данного пространства. Разговоры и обмен файлами не выйдут за пределы выделенного пространства, что обеспечивает конфиденциальность приватного общения.\nВнутри выделенного виртуального пространства люди могут общаться при помощи голосового чата, могут обмениваться документами из облачных хранилищ (OneDrive, Google drive…), просматривать их, комментировать и обсуждать.\nВ данный момент сервис находится на стадии открытого тестирования.",
+  "images": ["https://files.rtuitlab.dev/landing_src/situation_center/1.png", "https://files.rtuitlab.dev/landing_src/situation_center/2.png"],
+  "videos": ["https://www.youtube.com/embed/HRFRQa3tMcI"],
+  "tags": ["VR", "Social"],
+  "tech": ["NodeJS", "ASP. net core", "Unreal Engine"],
+  "developers": ["Макущенко М.А.", "Иванов И.Е."],
+  "site": null,
+  "sourceCode": [],
+  "date": "02/03/2022"
+}, {
+  "title": "Шутер в виртуальной реальности",
+  "description": "\nШутер в формате виртуальной реальности, позволяющий оттачивать стрельбу из оружия разных видов и уворачиваться от вражеских пуль.\n\nВ игре можно бросить вызов сразу нескольким противникам, либо сначала потренироваться в спокойной обстановке. В таком режиме у пользователя есть возможность стрелять по бутылкам в баре и мишеням в тире.\n\nНа выбор представлено 5 моделей оружия, 4 локации и 3 уровня сложности.",
+  "images": ["https://user-images.githubusercontent.com/57914366/148579235-a4f9e25e-dc3c-49d8-b1a6-7dbabfa52a99.jpg", "https://user-images.githubusercontent.com/57914366/148580147-297f79e2-15e3-4b34-8108-9550982b0fe2.jpg", "https://user-images.githubusercontent.com/57914366/148580160-84ee7b40-83f9-439c-a37e-4e1184bd4dd5.jpg", "https://user-images.githubusercontent.com/57914366/148580059-71f366be-9f6c-44bf-a662-abf7e3e00003.jpg"],
+  "videos": [],
+  "tags": ["VR", "Game"],
+  "tech": ["Unity", "C#"],
+  "developers": ["Егоркин Н.Е.", "Шорин И.А."],
+  "site": null,
+  "sourceCode": [{
+    "name": "Исходный код",
+    "link": "https://github.com/RTUITLab/VRDuel"
+  }],
+  "date": "02/03/2022"
+}, {
   "title": "Plane crash simulator",
   "description": "Симулятор, помещающий пользователя в ситуацию, когда он находится в падающем самолете",
   "images": ["https://github.com/RTUITLab/PlaneCrashSimulator/raw/master/landing/1.png", "https://github.com/RTUITLab/PlaneCrashSimulator/raw/master/landing/2.png", "https://github.com/RTUITLab/PlaneCrashSimulator/raw/master/landing/3.png"],
@@ -254,30 +279,19 @@ var data = [{
   }],
   "date": "02/03/2022"
 }, {
-  "title": "Шутер в виртуальной реальности",
-  "description": "\nШутер в формате виртуальной реальности, позволяющий оттачивать стрельбу из оружия разных видов и уворачиваться от вражеских пуль.\n\nВ игре можно бросить вызов сразу нескольким противникам, либо сначала потренироваться в спокойной обстановке. В таком режиме у пользователя есть возможность стрелять по бутылкам в баре и мишеням в тире.\n\nНа выбор представлено 5 моделей оружия, 4 локации и 3 уровня сложности.",
-  "images": ["https://user-images.githubusercontent.com/57914366/148579235-a4f9e25e-dc3c-49d8-b1a6-7dbabfa52a99.jpg", "https://user-images.githubusercontent.com/57914366/148580147-297f79e2-15e3-4b34-8108-9550982b0fe2.jpg", "https://user-images.githubusercontent.com/57914366/148580160-84ee7b40-83f9-439c-a37e-4e1184bd4dd5.jpg", "https://user-images.githubusercontent.com/57914366/148580059-71f366be-9f6c-44bf-a662-abf7e3e00003.jpg"],
+  "title": "BunnyHuntVR - Web VR шутер",
+  "description": "VR игра, обороняйте грядки от проворных и назойливых зайцев. Узнайте сколько морковки вы сможете сохранить.",
+  "images": ["https://github.com/RTUITLab/WebVRShoot/raw/master/LANDING/BunnyHuntVR.png", "https://github.com/RTUITLab/WebVRShoot/raw/master/LANDING/BunnyHuntVR2.png"],
   "videos": [],
-  "tags": ["VR", "Game"],
-  "tech": ["Unity", "C#"],
-  "developers": ["Егоркин Н.Е.", "Шорин И.А."],
-  "site": null,
+  "tags": ["VR", "Game", "Shooter"],
+  "tech": ["A-Frame", "Javascript", "Html"],
+  "developers": ["Соколов А.В", "Иерусалимов И.Д."],
+  "site": "https://webvrshoot.rtuitlab.dev/",
   "sourceCode": [{
-    "name": "Исходный код",
-    "link": "https://github.com/RTUITLab/VRDuel"
+    "name": "Проект",
+    "link": "https://github.com/RTUITLab/WebVRShoot"
   }],
-  "date": "02/03/2022"
-}, {
-  "title": "Виртуальный ситуационный центр",
-  "description": "Виртуальный ситуационный центр позволяет множеству людей в любой точке планеты взаимодействовать между собой в единой среде виртуальной реальности. При этом возможно использование любого доступного на данный момент шлема виртуальной реальности. Также сервисом можно пользоваться без шлема, используя клавиатуру и мышь.\nВ сервисе реализована концепция комнат, к которым подключаются люди, и их общение происходит только внутри данного пространства. Разговоры и обмен файлами не выйдут за пределы выделенного пространства, что обеспечивает конфиденциальность приватного общения.\nВнутри выделенного виртуального пространства люди могут общаться при помощи голосового чата, могут обмениваться документами из облачных хранилищ (OneDrive, Google drive…), просматривать их, комментировать и обсуждать.\nВ данный момент сервис находится на стадии открытого тестирования.",
-  "images": ["https://files.rtuitlab.dev/landing_src/situation_center/1.png", "https://files.rtuitlab.dev/landing_src/situation_center/2.png"],
-  "videos": ["https://www.youtube.com/embed/HRFRQa3tMcI"],
-  "tags": ["VR", "Social"],
-  "tech": ["NodeJS", "ASP. net core", "Unreal Engine"],
-  "developers": ["Макущенко М.А.", "Иванов И.Е."],
-  "site": null,
-  "sourceCode": [],
-  "date": "02/03/2022"
+  "date": "25/01/2022"
 }, {
   "title": "VRTherapy - Приложение для релаксации в VR со множеством разнообразных активностей",
   "description": "В VRTherapy вы можете погрузиться в захватывающую и успокаивающую виртуальную реальность с приятным звуковым сопровождением.\n\nОтдохните у виртуального костра, прогуляйтесь по летнему лесу, послушайте шум прибоя или же сыграйте в мини-игру, где вам предстоит управлять бумажным самолётиком на ветру.\n\nДанная игра предназначена исключительно для запуска в портативном VR-шлеме для Android, например, Google Cardboard.",
@@ -292,20 +306,6 @@ var data = [{
     "link": "https://github.com/RTUITLab/VRTherapy"
   }],
   "date": "02/03/2022"
-}, {
-  "title": "BunnyHuntVR - Web VR шутер",
-  "description": "VR игра, обороняйте грядки от проворных и назойливых зайцев. Узнайте сколько морковки вы сможете сохранить.",
-  "images": ["https://github.com/RTUITLab/WebVRShoot/raw/master/LANDING/BunnyHuntVR.png", "https://github.com/RTUITLab/WebVRShoot/raw/master/LANDING/BunnyHuntVR2.png"],
-  "videos": [],
-  "tags": ["VR", "Game", "Shooter"],
-  "tech": ["A-Frame", "Javascript", "Html"],
-  "developers": ["Соколов А.В", "Иерусалимов И.Д."],
-  "site": "https://webvrshoot.rtuitlab.dev/",
-  "sourceCode": [{
-    "name": "Проект",
-    "link": "https://github.com/RTUITLab/WebVRShoot"
-  }],
-  "date": "25/01/2022"
 }];
 var _default = data;
 exports.default = _default;
@@ -705,4 +705,4 @@ gallery.setActiveView(Math.floor(_projectsData.default.length / 2));
 updateProjectsInfo();
 document.getElementById("projects__tags__all").checked = true;
 },{"../data/projectsData":"rSus","../gallery/gallery":"Z6W6"}]},{},["TB2l"], null)
-//# sourceMappingURL=projects.d42ffa90.js.map
+//# sourceMappingURL=projects.a0407dfe.js.map
