246712966de694d04d733fb73b694999783e1ecf
Kirill <kirill.baikin@gmail.com>
2022-03-24 00:12:38 +0300

    f

--- jakeFunctions.js
+++ jakeFunctions.js
@@ -134,7 +134,6 @@ function downloadImages(i, j) {
 
     let result = [];
     let res = exec(`curl -H "Authorization: token ${process.env.token}" --ssl-no-revoke --header 'Accept: application/vnd.github.v3.raw' -o ./src/images/projects/${i}/${j.match(/([^\/]*)$/gim)[0]} --location https://api.github.com/repos/rtuitlab/${i}/contents/${j}?ref=master`, (error, stdout, stderror) => {
-      console.log(i, j,stdout,error,stderror);
 
       try {
         let buff = JSON.parse(stdout);
@@ -144,6 +143,7 @@ function downloadImages(i, j) {
           result.push(secondDownloadMethod(i, j).then(resolve));
         }
       } catch (e) {
+        console.log(i, j,stdout,error,stderror);
         result.push(secondDownloadMethod(i, j).then(resolve));
       }
       Promise.allSettled(result).then((e) => {
