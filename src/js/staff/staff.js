3e6807b096e350921a9c4a6e288e224847315f5b
Kirill <kirill.baikin@gmail.com>
2022-03-25 22:28:20 +0300

    f

--- src/js/staff/staff.js
+++ src/js/staff/staff.js
@@ -1,6 +1,5 @@
 let swiper = new Swiper('.staff__swiper', {
   spaceBetween: 10, slidesPerView: 'auto', freeMode: true, loop: true, loopFillGroupWithBlank: true, autoplay: {
     delay: 0, disableOnInteraction: false, pauseOnMouseEnter: true,
-  }, speed: 2200,preloadImages: false,
-  lazy: true,watchSlidesProgress:true
+  }, speed: 2200,
 });
