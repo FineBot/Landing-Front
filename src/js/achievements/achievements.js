6a3d80858fdfd2959321c7f3b75ff80fd4660bdf
Kirill <kirill.baikin@gmail.com>
2022-03-26 19:24:04 +0300

    f

--- src/js/achievements/achievements.js
+++ src/js/achievements/achievements.js
@@ -1,5 +1,7 @@
-var swiper = new Swiper(".achievements__swiperParent", {
-	slidesPerView: "auto", spaceBetween: 15, pagination: {
-		el: ".achievements__swiperPagination", clickable: true,
-	},
-});
+setTimeout(()=>{
+	var swiper = new Swiper(".achievements__swiperParent", {
+		slidesPerView: "auto", spaceBetween: 15, pagination: {
+			el: ".achievements__swiperPagination", clickable: true,
+		},
+	});
+},15)
--- src/js/staff/staff.js
+++ src/js/staff/staff.js
@@ -1,5 +1,7 @@
-let swiper = new Swiper('.staff__swiper', {
-  spaceBetween: 10, slidesPerView: 'auto', freeMode: true, loop: true, loopFillGroupWithBlank: true, autoplay: {
-    delay: 0, disableOnInteraction: false, pauseOnMouseEnter: true,
-  }, speed: 2200,
-});
+window.addEventListener('load', function() {
+  new Swiper('.staff__swiper', {
+    spaceBetween: 10, slidesPerView: 'auto', freeMode: true, loop: true, loopFillGroupWithBlank: true, autoplay: {
+      delay: 0, disableOnInteraction: false, pauseOnMouseEnter: true,
+    }, speed: 2200,
+  });
+}, { passive: true });
